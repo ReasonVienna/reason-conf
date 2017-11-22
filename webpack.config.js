9234cee311ab04677e3130bea209f3f37c157dca
Patrick Stapfer <ryyppy@users.noreply.github.com>
2017-11-22 14:59:03 +0100

    Use a public dir for publishing

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.bs.js',
+  entry: './lib/js/src/index.js',
   output: {
-    path: path.join(__dirname, "build"),
-    filename: 'index.js',
+    path: path.join(__dirname, "public"),
+    filename: 'app.js',
   },
 };
