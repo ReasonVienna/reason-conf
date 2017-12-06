0a5d10ceba1f79c17d82fd093d5441a03b73894f
Nik Graf <nik@nikgraf.com>
2017-12-06 19:35:40 +0100

    add gatsby to dev depedendencies and switch to yarn

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Reason Conf 2018`
   },
-  plugins: [`gatsby-plugin-react-helmet`],
-}
+  plugins: [`gatsby-plugin-react-helmet`]
+};
