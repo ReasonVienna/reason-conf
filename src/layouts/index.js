4571824679c0fea82cfc6e9b36c844d9e21676d3
Nik Graf <nik@nikgraf.com>
2017-12-06 19:41:50 +0100

    basic layout

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,22 +1,22 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Link from 'gatsby-link'
-import Helmet from 'react-helmet'
+import React from 'react';
+import PropTypes from 'prop-types';
+import Link from 'gatsby-link';
+import Helmet from 'react-helmet';
 
-import './index.css'
+import './index.css';
 
 const Header = () => (
   <div
     style={{
       background: 'rebeccapurple',
-      marginBottom: '1.45rem',
+      marginBottom: '1.45rem'
     }}
   >
     <div
       style={{
         margin: '0 auto',
         maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
+        padding: '1.45rem 1.0875rem'
       }}
     >
       <h1 style={{ margin: 0 }}>
@@ -24,15 +24,15 @@ const Header = () => (
           to="/"
           style={{
             color: 'white',
-            textDecoration: 'none',
+            textDecoration: 'none'
           }}
         >
-          Gatsby
+          Reason Conf 2018
         </Link>
       </h1>
     </div>
   </div>
-)
+);
 
 const TemplateWrapper = ({ children }) => (
   <div>
@@ -40,7 +40,7 @@ const TemplateWrapper = ({ children }) => (
       title="ReasonConf 2018"
       meta={[
         { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
+        { name: 'keywords', content: 'sample, something' }
       ]}
     />
     <Header />
@@ -49,16 +49,12 @@ const TemplateWrapper = ({ children }) => (
         margin: '0 auto',
         maxWidth: 960,
         padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
+        paddingTop: 0
       }}
     >
       {children()}
     </div>
   </div>
-)
+);
 
-TemplateWrapper.propTypes = {
-  children: PropTypes.func,
-}
-
-export default TemplateWrapper
+export default TemplateWrapper;
---
+++ src/pages/coc.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Link from 'gatsby-link';
+
+const SecondPage = () => (
+  <div>
+    <h1>Code of Conduct</h1>
+    <p>TBD</p>
+    <Link to="/">Go back to the homepage</Link>
+  </div>
+);
+
+export default SecondPage;
