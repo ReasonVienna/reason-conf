b291d78bc0333b9b4db752bfbaedc227829b29db
Andrey Okonetchnikov <andrey@okonet.ru>
2018-01-10 12:26:51 +0100

    Add autoprefixer

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,15 @@
+const autoprefixer = require("autoprefixer");
 module.exports = {
   siteMetadata: {
     title: `Reason Conf 2018`
   },
-  plugins: [`gatsby-plugin-react-helmet`]
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-postcss-sass`,
+      options: {
+        postCssPlugins: [autoprefixer()]
+      }
+    }
+  ]
 };
