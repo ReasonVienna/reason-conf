fb0473e97a9323de9af6893208c47f39d9aa6fb5
Andrey Okonetchnikov <andrey@okonet.ru>
2018-01-10 12:54:27 +0100

    Use SCSS instead of CSS

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,7 +2,7 @@ import "normalize.css";
 import React from "react";
 import Link from "gatsby-link";
 import Helmet from "react-helmet";
-import "./index.css";
+import "./index.scss";
 import Footer from "../components/footer";
 
 const TemplateWrapper = ({ children }) => (
