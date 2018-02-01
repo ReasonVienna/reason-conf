f05442b84f219dc45f316bda8b30d75af33ecc63
Andrey Okonetchnikov <andrey@okonet.ru>
2018-02-01 16:33:36 +0100

    refactor: Format gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,18 +1,18 @@
 const autoprefixer = require("autoprefixer");
 module.exports = {
   siteMetadata: {
-    title: `Reason Conf 2018`
+    title: "Reason Conf 2018"
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
+    "gatsby-plugin-react-helmet",
     {
-      resolve: `gatsby-plugin-postcss-sass`,
+      resolve: "gatsby-plugin-postcss-sass",
       options: {
         postCssPlugins: [autoprefixer()]
       }
     },
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: "gatsby-plugin-google-analytics",
       options: {
         trackingId: "UA-112745828-1",
         anonymize: true
