991c758cb69279d97e50af835d31ee8832c111c1
Andrey Okonetchnikov <andrey@okonet.ru>
2018-03-20 15:44:09 +0100

    Add epxport script for speaker images

---
+++ scripts/speakers.js
@@ -0,0 +1,26 @@
+const puppeteer = require("puppeteer");
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto("http://localhost:8000/speaker");
+  const elements = await page.$$(
+    ".src-pages----speaker-module---teaser---1pAxE"
+  );
+  const promises = elements.map(async (el, index) => {
+    const clip = await el.boundingBox();
+    const name = await page.evaluate(
+      element => element.innerHTML,
+      await el.$("h3")
+    );
+    console.log(`Exporting ${name}...`);
+    return page.screenshot({
+      path: `speaker-${name}.png`,
+      clip
+    });
+  });
+  Promise.all(promises).then(async () => {
+    console.log("All speakers screenshots exported!");
+    await browser.close();
+  });
+})();
